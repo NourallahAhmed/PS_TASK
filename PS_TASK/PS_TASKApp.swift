b46e5f1ceaa7e0774942a18a530b5f4db1aaa136
NourallahAhmed <NourAllahahmed7001@gmail.com>
2022-06-20 07:25:02 -0700

    Initial commit

---
+++ PS_TASK/PS_TASKApp.swift
@@ -0,0 +1,17 @@
+//
+//  PS_TASKApp.swift
+//  PS_TASK
+//
+//  Created by NourAllah Ahmed on 20/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct PS_TASKApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
